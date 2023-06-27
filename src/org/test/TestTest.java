2f50632c53caef7c456183d6bf7ae14fc544383f
SitharaSS <sitharasherin6@gmail.com>
2023-06-27 13:38:39 +0530

    New code by Sithara

--- src/org/test/TestTest.java
+++ src/org/test/TestTest.java
@@ -4,4 +4,8 @@ public class TestTest {
 	public static void main(String[] args) {
 		System.out.println("Hi");
 	}
+	private void login() {
+		System.out.println("This is by Sithara branch");
+
+	}
 }
